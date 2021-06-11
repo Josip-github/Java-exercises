b6517a454cbcd34f64440c5258368b00a402cf11
Josip-github <josipcota1@gmail.com>
2021-06-11 06:21:32 +0200

    If statement

--- Java Exercises/src/Zadaci.java
+++ Java Exercises/src/Zadaci.java
@@ -1,9 +1,25 @@
+import javax.swing.JOptionPane;
 
 public class Zadaci {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//Zadatak 1:
+		//Write a Java program to get a number from the user 
+		//and print whether it is positive or negative
 
+		int a = Integer.parseInt(
+				JOptionPane.showInputDialog("Unesite jedan broj.")
+				);
+		
+		if (a > 0) {
+			System.out.println("Broj koji ste unijeli (" + a + ") je pozitivan.");
+		}else if(a == 0){
+			System.out.println("Broj koji ste unijeli "
+					+ "(" + a + ") nije ni pozitivan ni negativan.");
+		}else {
+			System.out.println("Broj koji ste unijeli (" + a + ") je negativan.");
+		}
 	}
 
 }
