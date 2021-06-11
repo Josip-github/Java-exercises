a7ed74fd7079c650437544e90b046f54fe8b2783
Josip-github <josipcota1@gmail.com>
2021-06-11 06:30:43 +0200

    Zadatak 2, If statement

--- Java Exercises/src/Zadaci.java
+++ Java Exercises/src/Zadaci.java
@@ -7,7 +7,7 @@ public class Zadaci {
 		//Zadatak 1:
 		//Write a Java program to get a number from the user 
 		//and print whether it is positive or negative
-
+		/* poèetak zadatka 1
 		int a = Integer.parseInt(
 				JOptionPane.showInputDialog("Unesite jedan broj.")
 				);
@@ -20,6 +20,35 @@ public class Zadaci {
 		}else {
 			System.out.println("Broj koji ste unijeli (" + a + ") je negativan.");
 		}
+		kraj zadatka 1*/
+		
+		//Zadatak 2
+		//Take three numbers from the user and print the greatest number.
+		
+		int broj1, broj2, broj3;
+		
+		broj1 = Integer.parseInt(
+				JOptionPane.showInputDialog("Unesite jedan broj.")
+				);
+		
+		broj2 = Integer.parseInt(
+				JOptionPane.showInputDialog("Unesite jedan broj.")
+				);
+		
+		broj3 = Integer.parseInt(
+				JOptionPane.showInputDialog("Unesite jedan broj.")
+				);
+		
+		if (broj1 > broj2 & broj1 > broj3) {
+			System.out.println("Najveci broj koji ste unijeli je: " + broj1);
+		}else if(broj2 > broj1 & broj2 > broj3){
+			System.out.println("Najveci broj koji ste unijeli je: " + broj2);
+		}else if(broj3 > broj1 & broj3 > broj2) {
+			System.out.println("Najveci broj koji ste unijeli je: " + broj3);
+		}else if(broj1 == broj2 & broj2 == broj3) {
+			System.out.println("Brojevi koje ste unijeli su jednaki.");
+		}
+		
 	}
 
 }
