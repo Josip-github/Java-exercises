c6f1ff1befe388c6b476128e9974845c8d00c44a
Josip-github <josipcota1@gmail.com>
2021-06-11 06:55:33 +0200

    Switch

--- Java Exercises/src/Zadaci.java
+++ Java Exercises/src/Zadaci.java
@@ -22,6 +22,7 @@ public class Zadaci {
 		}
 		kraj zadatka 1*/
 		
+		/*poèetak zadatka 2
 		//Zadatak 2
 		//Take three numbers from the user and print the greatest number.
 		
@@ -47,6 +48,44 @@ public class Zadaci {
 			System.out.println("Najveci broj koji ste unijeli je: " + broj3);
 		}else if(broj1 == broj2 & broj2 == broj3) {
 			System.out.println("Brojevi koje ste unijeli su jednaki.");
+		}
+		kraj zadatka 2*/
+		
+		//Zadatak 3
+		/*
+		 *Write a Java program that keeps a number from the user and 
+		 *generates an integer between 1 and 7 and displays the name of the weekday.
+		*/
+		
+		int dan = Integer.parseInt(
+				JOptionPane.showInputDialog("Unesite jedan broj izmeðu 1 i 7")
+				);
+		
+		switch(dan) {
+			case 1:
+				System.out.println("Ponedjeljak");
+				break;
+			case 2:
+				System.out.println("Utorak");
+				break;
+			case 3:
+				System.out.println("Srijeda");
+				break;
+			case 4:
+				System.out.println("Èetvrtak");
+				break;
+			case 5:
+				System.out.println("Petak");
+				break;
+			case 6:
+				System.out.println("Subota");
+				break;
+			case 7:
+				System.out.println("Nedjelja");
+				break;
+			default:
+				System.out.println("Pogrešan unos.");
+				break;
 		}
 		
 	}
