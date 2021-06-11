553a76fbe0ef627df3f056249cbd63f1265795de
Josip-github <josipcota1@gmail.com>
2021-06-11 09:06:17 +0200

    for loop

---
+++ Java Exercises/src/Zadaci2.java
@@ -0,0 +1,25 @@
+import javax.swing.JOptionPane;
+
+public class Zadaci2 {
+
+	public static void main(String[] args) {
+
+		/*
+		 * Zadatak 1:
+		 * 
+		 * Write a program in Java to display the first 10 natural numbers.
+		*/
+		
+		for(int i = 1; i <= 10; i++) {
+			System.out.println(i);
+		}
+		
+		
+		
+		
+		
+		
+	
+	}
+
+}
