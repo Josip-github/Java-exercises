0c4388fa0efb2da32f68fda63c6f995ab168eb07
Josip-github <josipcota1@gmail.com>
2021-06-14 12:32:43 +0200

    Program ispisuje prvih 5 neparnih brojeva i njihov zbroj.

--- Java Exercises/src/Zadaci4.java
+++ Java Exercises/src/Zadaci4.java
@@ -2,7 +2,23 @@
 public class Zadaci4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//Write a program in Java to display the n terms of odd natural number
+		//and their sum.
+		
+		int sum = 0;
+		
+		for(int i = 0; i < 10; i++) {
+			if (i % 2 == 1) {
+				System.out.println(i);
+				sum += i;
+			}
+			
+		}
+		
+		System.out.println(sum);
+		
+		
 
 	}
 
