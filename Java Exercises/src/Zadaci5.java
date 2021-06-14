a1a98e66046b31f9ab70efe1d76cee47168e6c9b
Josip-github <josipcota1@gmail.com>
2021-06-14 12:34:52 +0200

    Initial commit

---
+++ Java Exercises/src/Zadaci5.java
@@ -0,0 +1,13 @@
+
+public class Zadaci5 {
+
+	public static void main(String[] args) {
+		
+		//Korisnik unosom odreðuje koliko prvih neparnih brojeva
+		//program treba ispisati te potom njihov zbroj
+		
+		
+
+	}
+
+}
