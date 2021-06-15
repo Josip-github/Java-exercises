721d9d77c7f90f75f4abdb90d536dfb1b2598fa7
Josip-github <josipcota1@gmail.com>
2021-06-15 17:07:44 +0200

    Ako želimo znakove dobivene u obrnutom nizu dodijeliti novoj String varijabli String reverse, onda to ovako napravimo

--- Java Exercises/src/for_loops/Exercise10.java
+++ Java Exercises/src/for_loops/Exercise10.java
@@ -9,10 +9,13 @@ public class Exercise10 {
 		//Writing a program that displays the reverse of a string.
 		
 		String str = JOptionPane.showInputDialog("Upišite jednu rijeè");
+		String reverse = "";
 		
 		for(int i = str.length() - 1; i >= 0; i--) {
-			System.out.print(str.charAt(i));
+			reverse += str.charAt(i);
 		}
+		
+		System.out.println(reverse);
 
 	}
 
