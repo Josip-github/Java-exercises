442adce0515bc95f308aeba411e7f6661a0d4f3a
Josip-github <josipcota1@gmail.com>
2021-06-15 21:15:01 +0200

    Palindrome
    Zanimljivo je primijetiti da ovaj loop ima dvije početne vrijednosti i u
    isto vrijeme inkrement i dekrement

---
+++ Java Exercises/src/for_loops/Exercise11.java
@@ -0,0 +1,31 @@
+package for_loops;
+
+public class Exercise11 {
+
+	public static void main(String[] args) {
+		
+		//Writing a program that finds if a string is a palindrome string or not.
+		// palindrome je rijeè koja isto izgleda neovisno èitamo li ju slijeva
+		// ili zdesna (neven, Ana, anavolimilovana, melem,...)
+		
+		String str = "zarraz";
+		boolean isPalindrome = true;
+		
+		for(int i = 0, j = str.length() - 1; i < j; i++, j--) {
+			if (str.charAt(i) == str.charAt(j)) {
+				continue;
+			}else {
+				isPalindrome = false;
+				break;
+			}
+		}
+		
+		if (isPalindrome) {
+			System.out.println(str + " is palindrome");
+		}else {
+			System.out.println(str + " is not palindrome.");
+		}
+
+	}
+
+}
