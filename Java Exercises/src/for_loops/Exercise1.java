e6a7efb81d733ec8482d8e0bcaa339729bb63b8d
Josip-github <josipcota1@gmail.com>
2021-06-15 07:18:57 +0200

    bez nule

--- Java Exercises/src/for_loops/Exercise1.java
+++ Java Exercises/src/for_loops/Exercise1.java
@@ -7,6 +7,9 @@ public class Exercise1 {
 		//Printing the even numbers between 1 to 100 with the help of loops.
 		
 		for(int i = 0; i <= 100; i++) {
+			if (i == 0) {
+				continue;
+			}
 			if (i % 2 == 0) {
 				System.out.println(i);
 			}
