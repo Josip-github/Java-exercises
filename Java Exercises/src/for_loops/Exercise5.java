22273925fb36e5259a10b9cc9b761d09ac6b4d05
Josip-github <josipcota1@gmail.com>
2021-06-15 09:57:15 +0200

    Broj je prime ako je djeljiv samo sa samim sobom i sa 1. Zbog toga loop započinje sa 2  i ide do broj/2 da se otkrije je li taj broj djeljiv s još nekim brojem. Ako je, boolean varijabla postaje false i breakom zaustavljamo loop.

---
+++ Java Exercises/src/for_loops/Exercise5.java
@@ -0,0 +1,30 @@
+package for_loops;
+
+import javax.swing.JOptionPane;
+
+public class Exercise5 {
+
+	public static void main(String[] args) {
+		
+		// Writing a program that reads a positive number N from the user 
+		// then indicates if N is prime or not.
+		
+		// N is a prime number if its divisors are only 1 and N.
+		
+		int n = Integer.parseInt(
+				JOptionPane.showInputDialog("Unesite jedan cijeli broj.")
+				);
+
+		boolean isPrime = true;
+		
+		for(int i = 2; i <=n/2; i++) {
+			if (n % i == 0) {
+				isPrime = false;
+				break;
+			}
+		}
+		
+		System.out.println(isPrime ? "prime" : "not prime");
+	}
+
+}
