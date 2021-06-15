73f051d5fc1a556ad95ed7160783f320c93471d3
Josip-github <josipcota1@gmail.com>
2021-06-15 07:38:43 +0200

    Zadatak: Writing a program that reads a sequence of integers from the user and stops by displaying Done” when the sum of these values exceeds 100.

---
+++ Java Exercises/src/for_loops/Exercise3.java
@@ -0,0 +1,29 @@
+package for_loops;
+
+import javax.swing.JOptionPane;
+
+public class Exercise3 {
+
+	public static void main(String[] args) {
+		
+		// Writing a program that reads a sequence of integers
+		// from the user and stops by displaying Done” 
+		// when the sum of these values exceeds 100.
+
+		
+		
+		int sum = 0;
+		
+		for(int i = 0; i < 100; i++) {
+			int a = Integer.parseInt(
+					JOptionPane.showInputDialog("Unesite jedan cijeli broj.")
+					);
+			sum += a;
+			if(sum >= 100) {
+				System.out.println("Done.");
+				break;
+			}
+		}
+	}
+
+}
