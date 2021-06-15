15610543849a8c2bca40bc0b2047251775027883
Josip-github <josipcota1@gmail.com>
2021-06-15 07:21:23 +0200

    Ovo je drugi način rješenja ako ne želim nulu; inicijaliziram i = 1

--- Java Exercises/src/for_loops/Exercise1.java
+++ Java Exercises/src/for_loops/Exercise1.java
@@ -6,10 +6,7 @@ public class Exercise1 {
 		
 		//Printing the even numbers between 1 to 100 with the help of loops.
 		
-		for(int i = 0; i <= 100; i++) {
-			if (i == 0) {
-				continue;
-			}
+		for(int i = 1; i <= 100; i++) {
 			if (i % 2 == 0) {
 				System.out.println(i);
 			}
