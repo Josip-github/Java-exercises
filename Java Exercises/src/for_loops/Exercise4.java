48d3831687ac1f9bb7c7ee432bacdd6489d0fcaa
Josip-github <josipcota1@gmail.com>
2021-06-15 07:50:15 +0200

    Zadatak: Writing a program that displays the sum of the strict divisors of an integer given by the user.

---
+++ Java Exercises/src/for_loops/Exercise4.java
@@ -0,0 +1,14 @@
+package for_loops;
+
+public class Exercise4 {
+
+	public static void main(String[] args) {
+		
+		// Writing a program that displays the sum of the strict divisors
+		// of an integer given by the user.
+		
+		
+
+	}
+
+}
