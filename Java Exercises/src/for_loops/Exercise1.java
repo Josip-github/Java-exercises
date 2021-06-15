a888facc1709ec6e8ef3fae2698b2be043634115
Josip-github <josipcota1@gmail.com>
2021-06-15 07:16:08 +0200

    Zadatak

---
+++ Java Exercises/src/for_loops/Exercise1.java
@@ -0,0 +1,13 @@
+package for_loops;
+
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		
+		//Printing the even numbers between 1 to 100 with the help of loops.
+		
+		
+
+	}
+
+}
