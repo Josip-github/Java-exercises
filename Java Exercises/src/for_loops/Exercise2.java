ae00031716d2caab960106ec7043fa761b3c2e9e
Josip-github <josipcota1@gmail.com>
2021-06-15 07:23:55 +0200

    Zadatak 2: Printing the odd numbers between 1 to 100 with the help of loops.

---
+++ Java Exercises/src/for_loops/Exercise2.java
@@ -0,0 +1,16 @@
+package for_loops;
+
+public class Exercise2 {
+
+	public static void main(String[] args) {
+		
+		//Printing the odd numbers between 1 to 100 with the help of loops.
+
+		for(int i = 0; i < 100; i++) {
+			if (i % 2 == 1) {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
