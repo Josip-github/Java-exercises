8b52745ed22f9bc00b4d1a2b95f47dc4e9a09c50
Josip-github <josipcota1@gmail.com>
2021-06-16 09:07:02 +0200

    Update zadatka
    prazan kvadrat

--- Java Exercises/src/for_loops/Exercise13.java
+++ Java Exercises/src/for_loops/Exercise13.java
@@ -19,6 +19,37 @@ public class Exercise13 {
 			}
 			System.out.println();
 		}
+		
+		System.out.println("\n");
+		
+		//UPDATE: sad treba napraviti takav kvadrat samo da je unutra prazan
+		// tj., ispisuju se sve zvjezdice samo u prvom i zadnjem redu, 
+		// u drugim redovima samo prva i zadnja. 
+		
+		for(int i = 1; i <= n; i++) {
+			if (i == 1 || i == n) {
+				
+				for(int j = 1; j <= n; j++) {
+					System.out.print("*");
+				}
+				System.out.println();
+				
+			}else {
+				for(int j = 1; j <= n; j++) {
+					if (j == 1 || j == n) {
+						System.out.print("*");
+						
+					}else {
+						System.out.print(" ");
+						
+					}
+					
+				}
+				System.out.println();
+			}
+			
+		}
+		
 
 	}
 
