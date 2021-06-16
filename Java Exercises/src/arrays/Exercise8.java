6239085b45e11cf1706ce3b602641e63a5210006
Josip-github <josipcota1@gmail.com>
2021-06-16 15:24:52 +0200

    Write a Java program to copy an array by iterating the array

---
+++ Java Exercises/src/arrays/Exercise8.java
@@ -0,0 +1,29 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class Exercise8 {
+
+	public static void main(String[] args) {
+		
+		// Write a Java program to copy an array by iterating the array.
+		
+		int [] intArray = {6,7,88,3,5,1,54};
+		int [] copyArray = new int[intArray.length];
+		
+		for(int i = 0; i < intArray.length; i++) {
+			System.out.print(intArray[i] + " ");
+		}
+		
+		System.out.println("\n***");
+		
+		for(int i = 0; i < intArray.length; i++) {
+			copyArray[i] = intArray[i];
+			System.out.print(copyArray[i] + " ");
+		}
+		
+		
+
+	}
+
+}
