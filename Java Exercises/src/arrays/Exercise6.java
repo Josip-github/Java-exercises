cf8a382a7b414ec2a5fe047dceae4bd10716955c
Josip-github <josipcota1@gmail.com>
2021-06-16 11:48:08 +0200

    Ispisivanje indeksa određenog elementa u nizu

---
+++ Java Exercises/src/arrays/Exercise6.java
@@ -0,0 +1,29 @@
+package arrays;
+
+import javax.swing.JOptionPane;
+
+public class Exercise6 {
+
+	public static void main(String[] args) {
+		
+		//Write a Java program to find the index of an array element.
+		
+		int [] intArray = {6,7,3,5,1,54};
+		
+		int n = Integer.parseInt(
+				JOptionPane.showInputDialog("Unesite jedan element iz niza intArray")
+				);
+		
+		for(int i = 0; i < intArray.length; i++) {
+			if (n == intArray[i]) {
+				System.out.println("Element koji ste unijeli - " + n + 
+						" - \nnalazi se na indexu: " + i);
+			}
+			
+		}
+		
+		
+
+	}
+
+}
