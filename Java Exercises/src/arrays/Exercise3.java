c4895c22f661f84d5a2d843c9c681e87242d6a7e
Josip-github <josipcota1@gmail.com>
2021-06-16 10:03:28 +0200

    Printing the square

---
+++ Java Exercises/src/arrays/Exercise3.java
@@ -0,0 +1,20 @@
+package arrays;
+
+public class Exercise3 {
+
+	public static void main(String[] args) {
+		
+		//Write a Java program to print the square.
+		
+		int [][] kvadrat = new int[10][10];
+		
+		for(int i = 0; i < 10; i++) {
+			for(int j = 0; j < 10; j++) {
+				System.out.print(kvadrat[i][j]);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
