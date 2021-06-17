4103aeffc8e813c114986382caecaa1c26aff837
Josip-github <josipcota1@gmail.com>
2021-06-17 09:25:36 +0200

    Objašnjenje: Ovako je još bolje, ne stvaram nepotrebne nove nizove, nego kreiram novu lokalnu varijablu int temp u koju spremam prvi dvostruki element koji želim izbrisati, a to je: niz[i]. A niz[i] dobiva vrijednost: 0.

--- Java Exercises/src/arrays/Exercise16.java
+++ Java Exercises/src/arrays/Exercise16.java
@@ -9,14 +9,13 @@ public class Exercise16 {
 		//Write a Java program to remove duplicate elements from an array.
 		
 		int niz[] = {1,4,56,87,56,43,1};
-		int niz2[] = new int[10];
-		int niz3[] = new int[10];
 		
 		for(int i = 0; i < niz.length; i++) {
 			for(int j = i + 1; j < niz.length; j++) {
 				if (niz[i] == niz[j]) {
-					niz2[i] = niz[i];
-					niz[i] = 0;		
+					int temp = niz[i];
+					niz[i] = 0;
+					
 				}
 				
 			}
