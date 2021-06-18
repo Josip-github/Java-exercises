32ace3a1278a5ea87731da0a21a039d1eee4d2dd
Josip-github <josipcota1@gmail.com>
2021-06-18 11:24:06 +0200

    Bolji način rješavanja zadatka:

--- Java Exercises/src/arrays/Exercise22.java
+++ Java Exercises/src/arrays/Exercise22.java
@@ -6,7 +6,7 @@ public class Exercise22 {
 
 		// Write a Java program to find a missing number in an array.
 		
-		int niz[] = {1,2,3,4,6,7,8,9,10};
+		/*int niz[] = {1,2,3,4,6,7,8,9,10};
 		
 		for(int i = 0; i < niz.length; i++) {
 			if (niz[i] == niz.length - 1) {
@@ -17,7 +17,35 @@ public class Exercise22 {
 			}else if (niz[i] != niz[i+1] - 1) {
 				System.out.println("A missing number is: " + (niz[i] + 1));
 			}
+		}*/
+		
+		
+		int niz2[] = {1,2,4,5,6,7,8};
+		
+		//Najveæi broj u ovom nizu je 8
+		
+		// formula je sljedeæa:
+		
+		//1+2+3+4+5+6+7+8 = 8 * ((8+1) / 2)
+		
+		//ako jedan element nedostaje, to æe u konaènom rezultatu biti vidljivo
+		
+		int maxElement = niz2[6];
+		
+		int expectedSumElements =  maxElement * (maxElement + 1) / 2;
+		
+		int sumArrayElements = 0;
+		
+		System.out.println("maxEl: " + maxElement);
+		
+		System.out.println("sumEl: " + expectedSumElements);
+		
+		for(int i = 0; i < niz2.length; i++) {
+			sumArrayElements += niz2[i];
 		}
+		
+		System.out.println("A missing element is: " +
+		(expectedSumElements - sumArrayElements));
 	}
 
 }
