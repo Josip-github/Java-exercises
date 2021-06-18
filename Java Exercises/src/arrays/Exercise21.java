e43cf376c36d477dbb56aea4a19366d32de6dae7
Josip-github <josipcota1@gmail.com>
2021-06-18 09:25:30 +0200

    Dodavanje uvjeta length Nizovi moraju biti iste duljine

--- Java Exercises/src/arrays/Exercise21.java
+++ Java Exercises/src/arrays/Exercise21.java
@@ -8,13 +8,13 @@ public class Exercise21 {
 
 		// Write a Java program to test the equality of two arrays.
 		
-		int niz1[] = {5,8,3,9,11};
-		int niz2[] = {1,8,3,9,11};
+		int niz1[] = {5,8,3,9,11,0};
+		int niz2[] = {5,8,3,9,11};
 		
 		boolean equal = true;
 		
 		for(int i = 0; i < niz1.length; i++) {
-			if (niz1[i] == niz2[i]) {
+			if (niz1.length == niz2.length && niz1[i] == niz2[i]) {
 				equal = true;
 			}else {
 				equal = false;
