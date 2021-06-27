f126654bd9e430901151e76709dc89c7870de5df
Josip-github <josipcota1@gmail.com>
2021-06-27 16:25:05 +0200

    Exercise 1
    Write a Java program to get the character at the given index within the
    String.

---
+++ Java Exercises/src/string/Exercise01.java
@@ -0,0 +1,16 @@
+package string;
+
+public class Exercise01 {
+
+	public static void main(String[] args) {
+
+		// Write a Java program to get the character at the given index within the String.
+		
+		String str = "Java Exercises";
+		
+		System.out.println("Original string is: " + str);
+		System.out.println("The character at the position 0 is: " + str.charAt(0));
+		System.out.println("The character at the position 10 is: " + str.charAt(10));
+	}
+
+}
