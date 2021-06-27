034039cdc2cc8b06e2a54512b5debcf00d80d4b9
Josip-github <josipcota1@gmail.com>
2021-06-27 16:51:35 +0200

    Exercise 6
    Write a Java program to compare two strings lexicographically, ignoring
    case differences.

---
+++ Java Exercises/src/string/Exercise06.java
@@ -0,0 +1,25 @@
+package string;
+
+public class Exercise06 {
+	
+	// Write a Java program to compare two strings lexicographically,
+	// ignoring case differences.
+	
+	private static void compareStrings(String str1, String str2) {
+		
+		int comparison = str1.compareToIgnoreCase(str2);
+		
+		if (comparison == 0) {
+			System.out.println("Stringovi su jednaki,"
+					+ " bez obzira na nejednakosti u velièini slova");
+		}else {
+			System.out.println("Stringovi nisu jednaki.");
+		}
+	}
+
+	public static void main(String[] args) {
+
+		compareStrings("Pobjeda", "pobjeda");
+	}
+
+}
