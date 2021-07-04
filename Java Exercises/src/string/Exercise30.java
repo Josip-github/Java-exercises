2e982084fa83504639912690c3916655af62ab6f
Josip-github <josipcota1@gmail.com>
2021-07-04 10:43:01 +0200

    Exercise 30
    Write a Java program to convert all the characters in a string to
    uppercase.

---
+++ Java Exercises/src/string/Exercise30.java
@@ -0,0 +1,14 @@
+package string;
+
+public class Exercise30 {
+	
+	// Write a Java program to convert all the characters in a string to uppercase. 
+
+	public static void main(String[] args) {
+
+		String str1 = "All we have to decide is what to do with the time that is given to us.";
+		
+		System.out.println(str1.toUpperCase());
+	}
+
+}
