d6f1ad8e3f4eed4efc4433cd2819fa6ce6bcc9fb
Josip-github <josipcota1@gmail.com>
2021-07-04 07:51:01 +0200

    Exercise 22
    Write a Java program to get the length of a given string.

---
+++ Java Exercises/src/string/Exercise22.java
@@ -0,0 +1,21 @@
+package string;
+
+public class Exercise22 {
+	
+	// Write a Java program to get the length of a given string.
+	
+	private static int getLengthOfString(String str1) {
+		int len = str1.length();
+		return len;
+	}
+
+	public static void main(String[] args) {
+
+	System.out.println(getLengthOfString(
+			"Rindfleischetikettierungsüberwachungsaufgabenübertragungsgesetz"));
+	
+	// Zakon o prijenosu nadzornih obaveza o etiketiranju govedjeg mesa :))
+		
+	}
+
+}
