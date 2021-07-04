eda74241099445cdb89f6db2cdb43803fda8d5c1
Josip-github <josipcota1@gmail.com>
2021-07-04 10:40:39 +0200

    Exercise 29
    Write a Java program to convert all the characters in a string to
    lowercase.

---
+++ Java Exercises/src/string/Exercise29.java
@@ -0,0 +1,16 @@
+package string;
+
+public class Exercise29 {
+	
+	// Write a Java program to convert all the characters in a string to lowercase.
+
+	public static void main(String[] args) {
+		
+		String str1 = "A WIZARD IS NEVER LATE, NOR IS HE EARLY."
+				+ " HE ARRIVES PRECISELY WHEN HE MEANS TO.";
+		
+		System.out.println(str1.toLowerCase());
+
+	}
+
+}
